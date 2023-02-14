0a7c8f45059cb356eb4ddf198084e080482cca24
matt Mcentire <Msmcentire1220@outlook.com>
2023-02-14 02:42:39 -0700

    Changed naming convention on jest tests

--- index.test.js
+++ index.test.js
@@ -5,21 +5,23 @@ const validTestOne = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 
 describe("IsDivisibleBy2Tests", () => {
 
-    test("Given a num that is divisible by 2 and it equals 0, returns true", () => {
+    test("IsDivisibleBy2_NumDivisibleBy2_ReturnsTrue", () => {
         expect(index.isDivisibleBy2(2)).toBeTruthy();
     }); 
 
-    test("Given a number that is NOT divisible by 2 and it does not equal 0, returns false", () => {
+    test("IsDivisibleBy2_NumNotDisibleBy2_ReturnsFalse", () => {
         expect(index.isDivisibleBy2(13)).toBeFalsy();
-    })
+    });
 
 });
 
 
 // describe("validateCredTests", () => {
+
 //     test("function that validates account numbers and returns if they are valid.", () => {
-//         expect(validateCred(validTestZero)).tobeTruthy();
+//         expect(index.validateCred(validTestZero)).tobeTruthy();
 //     });
+
 // });
 
 

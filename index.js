0924fad6fece531dc737d72e93201db1c5949c4d
matt Mcentire <Msmcentire1220@outlook.com>
2023-02-16 09:04:59 -0700

    Add coverage, add tests around new methods

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-// drop last digit in array 
+// drop last digit in array
 
-const isDivisibleBy2 = (num) => {
-    return num % 2 === 0;
+const doubleEveryOtherDigitStartingFromMostRightInArray = (accountNumber) => {
+    console.log(accountNumber);
+    let newAccountNumber = new Array();
+    let index = accountNumber.length - 1;
+    for(let i = index; i >= 0; i--){
+        let num = accountNumber[i];
+        if(i != index){
+            newAccountNumber.unshift(num);
+            continue;
+        }
+        num *= 2;
+        if(num > 9){
+            num -= 9;
+        }
+        index -= 2;
+        newAccountNumber.unshift(num);
+    }
+    return newAccountNumber;
 }
 
 const validateCred = (accountNumber) => {
-    accountNumber.pop();
+    var lastDigit = accountNumber.pop();
 
-    let doubled = accountNumber.map(num => {
-        if(isDivisibleBy2(accountNumber.indexOf(num))){
-            let result = num * 2;
-            // if(result > 9){
-            //     num -= 9;
-            // }
-            return result;
-        }
-        return num;
-    });
-    console.debug(doubled);
-
+    let newAccountNumber = doubleEveryOtherDigitStartingFromMostRightInArray(accountNumber)
+    console.debug(newAccountNumber);
 
     let sum = 0;
-    for(let number of doubled){
+    for(let number of newAccountNumber){
         sum += number;
     }
-    console.debug(`Sum: ${sum}`);
+    sum += lastDigit;
 
     return (sum % 10 === 0 ? true : false);
 }
@@ -32,5 +38,5 @@ const validateCred = (accountNumber) => {
 
 module.exports = {
     validateCred,
-    isDivisibleBy2 : isDivisibleBy2
+    doubleEveryOtherDigitStartingFromMostRightInArray
 };
--- index.test.js
+++ index.test.js
@@ -1,27 +1,70 @@
 const index = require("./index");
 
-const validTestZero = [4,5,3,9,6,8,9,8,8,7,7,0,5,7,9,8];
-const validTestOne = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 
-describe("IsDivisibleBy2Tests", () => {
+describe("doubleEveryOtherDigitStartingFromMostRightInArray", () =>{
 
-    test("IsDivisibleBy2_NumDivisibleBy2_ReturnsTrue", () => {
-        expect(index.isDivisibleBy2(2)).toBeTruthy();
-    }); 
+    test("doubleEveryOtherDigitStartingFromMostRightInArray_DigitsWhoseIndexAreDivisibleBy2_Doubled", () => {
+        expect.hasAssertions();
+        let accountNumber = [2, 6, 1, 6, 3, 9, 2];
+        let expectedNumber = [4, 6, 2, 6, 6, 9, 4];
 
-    test("IsDivisibleBy2_NumNotDisibleBy2_ReturnsFalse", () => {
-        expect(index.isDivisibleBy2(13)).toBeFalsy();
+        let actual = index.doubleEveryOtherDigitStartingFromMostRightInArray(accountNumber);
+
+        expect(new Set(actual)).toStrictEqual(new Set(expectedNumber));
     });
 
+    test("doubleEveryOtherDigitStartingFromMostRightInArray_DigitsWhoseDoubledAbove9_ReducedBy9", () => {
+        expect.hasAssertions();
+        let accountNumber = [7, 6, 1, 6, 9, 9, 2];
+        let expectedNumber = [5, 6, 2, 6, 9, 9, 4];
+
+        let actual = index.doubleEveryOtherDigitStartingFromMostRightInArray(accountNumber);
+
+        expect(new Set(actual)).toEqual(new Set(expectedNumber));
+    });
+
+    test("doubleEveryOtherDigitStartingFromMostRightInArray_ExtraLongArray_AllNumberIndexesDivisibleBy2AreDoubled", () => {
+        const accountNumber = [4, 5, 3, 9, 6, 8, 9, 8, 8, 7, 7, 0, 5, 7, 9, 8];
+        const expectedNumber =[4, 1, 3, 9, 6, 7, 9, 7, 8, 5, 7, 0, 5, 5, 9, 7];
+
+        let actual = new Set(index.doubleEveryOtherDigitStartingFromMostRightInArray(accountNumber));
+
+        expect(actual).toEqual(new Set(expectedNumber));
+    });
 });
 
 
-// describe("validateCredTests", () => {
+describe("validateCredTests", () => {
+    // All valid credit card numbers
+    const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+    const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+    const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+    const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+    const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
-//     test("function that validates account numbers and returns if they are valid.", () => {
-//         expect(index.validateCred(validTestZero)).tobeTruthy();
-//     });
+    //invalid numbers
+    const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+    const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+    const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+    const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+    const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
-// });
+    test("ValidateCred_ValidNumbers_ReturnsTrue", () => {
+        expect(index.validateCred(valid1)).toBeTruthy();
+        expect(index.validateCred(valid2)).toBeTruthy();
+        expect(index.validateCred(valid3)).toBeTruthy();
+        expect(index.validateCred(valid4)).toBeTruthy();
+        expect(index.validateCred(valid5)).toBeTruthy();
+    });
+
+    test("ValidateCred_InvalidNumbers_ReturnsFalse", () => {
+        expect(index.validateCred(invalid1)).toBeFalsy();
+        expect(index.validateCred(invalid2)).toBeFalsy();
+        expect(index.validateCred(invalid3)).toBeFalsy();
+        expect(index.validateCred(invalid4)).toBeFalsy();
+        expect(index.validateCred(invalid5)).toBeFalsy();
+    });
+
+});
 
 
